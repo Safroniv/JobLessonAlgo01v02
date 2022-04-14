f5d38fa4855db511673aca92374b8bf7a90d7d35
safroniv <safroniv@mail.ru>
2022-04-14 22:54:41 +0300

    GB - Алгоритмы Урок 1 Задание 3 - готово рекурсией.

--- JobLessonAlgo01v02Part03/Program.cs
+++ JobLessonAlgo01v02Part03/Program.cs
@@ -10,9 +10,26 @@ namespace JobLessonAlgo01v02Part03
     {
         static void Main(string[] args)
         {
-
-
+            //Написать программу, вычисляющую число Фибоначчи для заданного значения рекурсивным способом. 
+            int number01 = 5;
+            Console.WriteLine(GetFibonacci(number01));
+            int number02 = 10;
+            Console.WriteLine(GetFibonacci(number02));
+            int number03 = 20;
+            Console.WriteLine(GetFibonacci(number03));
+            int number04 = 0;
+            Console.WriteLine(GetFibonacci(number04));
+            int number05 = -1;
+            Console.WriteLine(GetFibonacci(number05));
         }
-
+        //Вычисление числа Фибоначчи рекурсивным способом
+        static int GetFibonacci(int number)
+        {
+            if (number == 0 || number == 1)
+            {
+                return number;
+            }
+            return GetFibonacci(number - 1) + GetFibonacci(number - 2);
+        }
     }
 }
